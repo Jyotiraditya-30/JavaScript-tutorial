bb9b51f74dd01c47ac9516f3e3905f419229ccee
Jyotiraditya Singh <jyotir2k1@gmail.com>
2024-03-22 12:23:54 +0530

    Ternary Operator tutorial

---
+++ TernaryOperator.js
@@ -0,0 +1,25 @@
+function getFee(isMember) {
+    return isMember ? '$2.00' : '$10.00';
+}
+
+// console.log(getFee(true));
+
+// console.log(getFee(false));
+
+// console.log(getFee(null));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+// console.log(beverage); 
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+const greeting = (person) => {
+    const name = person ? person.name : "stranger";
+    return `Howdy, ${name}`;
+};
+
+// console.log(greeting({ name: "Alice" }));
+// console.log(greeting(null)); 
