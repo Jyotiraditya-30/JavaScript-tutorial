475aa4b20555614fd9dbc017c793e34677999d8a
Jyotiraditya Singh <jyotir2k1@gmail.com>
2025-07-22 22:14:27 +0530

    02_DataTypes.js

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,59 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using nodejs not browser
+
+let name = "Jyotir" // string
+let age = 23 // number
+let isSubscribed = false // boolean
+
+// ++++++++ Data Types +++++++++++++ //
+
+// Primitive Data type
+
+/*
+number => 2^53
+bigint
+string = ""
+boolean => true/false
+null => standalone value
+undefined => value isnt assigned to variable
+symbol => unique
+*/
+console.log(typeof null)
+console.log(typeof undefined)
+// Non Primitive Data type
+
+/*
+object => {}
+*/
+
+// ++++++++++++++++++++++++++++++++++++++ Type Conversion ++++++++++++++++++++++++++++++++ //
+
+/*
+let score = "33aaa"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber)
+*/
+
+/*
+"33" => 33
+"33abc" => Nan
+null => 0
+true/false => 1/0
+*/
+
+ /*
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+1 => true; 0 => false
+"" => false
+"hitesh" ==> true
+*/
+
