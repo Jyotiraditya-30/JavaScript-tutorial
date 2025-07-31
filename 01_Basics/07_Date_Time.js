98ffd434fb86790fd1afc56e83548e3354abef84
Jyotiraditya Singh <jyotir2k1@gmail.com>
2025-07-31 16:52:18 +0530

    Update

---
+++ 01_Basics/07_Date_Time.js
@@ -0,0 +1,9 @@
+let newDate = new Date();
+console.log("Time", newDate);
+console.log(newDate.getDate())
+console.log(newDate.getTime()/1000)
+console.log(newDate.getFullYear())
+console.log(newDate.getHours())
+
+console.log(newDate.toLocaleString())
+
